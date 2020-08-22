67d114356a254a8e7538e9b04956b4c9d85bed4f
BhuvaneshwarM <bhuvaneshwar.m@rently.com>
2020-08-22 13:24:31 +0530

    Class declaration and definitions are separated

--- Cpp/Queue/queue.cpp
+++ Cpp/Queue/queue.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //Linked list
 
 
-//Node class
+/*Node class definition*/
 template <typename T>
 class Node{
 public:
@@ -15,27 +15,39 @@ public:
         
         ~Node() {}
         
-        std::shared_ptr<Node<T>> getNext(){
-            return node_next;
-        }
+        std::shared_ptr<Node<T>> getNext();
         
-        void setNext(std::shared_ptr<Node<T>>& next){
-            node_next = next;
-        }
+        void setNext(std::shared_ptr<Node<T>>& next);
         
-        void delNext(){
-            node_next = NULL;
-        }
-        
-        
-        T getValue(){
-            return node_val;
-        }
+        void delNext();
+        T getValue();
         
 private:
 	T node_val;
 	std::shared_ptr<Node<T>> node_next;
 };
+
+
+template <typename T>
+std::shared_ptr<Node<T>> Node<T>::getNext(){
+     return node_next;
+}
+
+template <typename T>
+void Node<T>::setNext(std::shared_ptr<Node<T>>& next){
+    node_next = next;
+}
+
+template <typename T>
+void Node<T>::delNext(){
+    node_next = NULL;
+}
+
+template <typename T>
+T Node<T>::getValue(){
+    return node_val;
+}
+
 
 //print function
 template<typename T>
@@ -49,98 +61,134 @@ void print(shared_ptr<Node<T>>& node){
     cout<<endl;
 };
 
+
+
+/*Linked list class definition*/
 template <typename T>
 class LinkedList{
     private:
         shared_ptr<Node<T>> head = NULL;
         shared_ptr<Node<T>> tail = NULL;
         
-        bool addLast(shared_ptr<Node<T>>& node){
-            if(head == NULL){
-                head  = node;
-                tail = head;
-                return true;
-            }
-            
-            shared_ptr<Node<T>> temp = tail;
-            tail->setNext(node);
-            tail = node;
-            return true;
-        }
-        
-        bool popFirst(){
-            head = head->getNext();
-            return true;
-        }
+        bool addLast(shared_ptr<Node<T>>& node);
+        bool popFirst();
         
     public:
         LinkedList(){}
         ~LinkedList(){};
         
         
-        bool addLast(T val){
-            auto new_node = make_shared<Node<T>>(val);
-            return addLast(new_node);
-        }
+        bool addLast(T val);
         
-        T * popFront(){
-            T *valptr;
-            if(isEmpty()){
-                valptr = nullptr;
-            } else {
-                auto val = head->getValue();
-                valptr = &val;
-                popFirst();
-            }
-            return valptr;
-        }
+        T * popFront();
         
-        bool isEmpty(){
-            if(head == NULL){
-                return true;
-            }
-            return false;
-        }
+        bool isEmpty();
         
-        void print(){
-            cout<<"BACK| <-";
-            auto temp=head;
-            while(temp != nullptr){
-            cout<<temp->getValue()<<"<-";
-            temp = temp->getNext();
-            }
-            cout<<"| FRONT";
-            cout<<endl;
-        
-        };
+        void print();
         
 };
 
+template <typename T>
+bool LinkedList<T>::addLast(shared_ptr<Node<T>>& node){
+            if(head == NULL){
+                head  = node;
+                tail = head;
+                return true;
+        }
+            
+            shared_ptr<Node<T>> temp = tail;
+            tail->setNext(node);
+            tail = node;
+            return true;
+};
 
-template <typename T>  //Queue class for LL Implementation
+template <typename T>
+bool LinkedList<T>:: popFirst(){
+    head = head->getNext();
+    return true;
+};
+
+template <typename T>
+bool LinkedList<T>:: addLast(T val){
+    auto new_node = make_shared<Node<T>>(val);
+    return addLast(new_node);
+};
+
+template <typename T>
+T* LinkedList<T>::popFront(){
+    T *valptr;
+    if(isEmpty()){
+        valptr = nullptr;
+    } else {
+        auto val = head->getValue();
+        valptr = &val;
+        popFirst();
+    }
+    return valptr;
+};
+
+template <typename T>
+bool LinkedList<T>::isEmpty(){
+    if(head == NULL){
+        return true;
+    }
+    return false;
+};
+
+template <typename T>
+void LinkedList<T>::print(){
+    cout<<"BACK| <-";
+    auto temp=head;
+    while(temp != nullptr){
+        cout<<temp->getValue()<<"<-";
+        temp = temp->getNext();
+    }
+    cout<<"| FRONT";
+    cout<<endl;
+};
+
+
+
+/*Queue class using LL Implementation*/
+template <typename T>
 class QueueLinkedList{
     public:
 
         LinkedList<T> LL;
-        bool enqueue(T val){
-            return LL.addLast(val);
-        }
+        bool enqueue(T val);
         
-        T * dequeue(){
-            //auto value = LL.popFront();
-            return LL.popFront();
-        }
+        T * dequeue();
         
-        bool isEmpty(){
-            return LL.isEmpty();
-        }
-        void print(){
-            LL.print();
-        }
+        bool isEmpty();
+
+        void print();
         
 };
 
-template <typename T> //Queue class for array implementation
+template <typename T>
+bool QueueLinkedList<T>::enqueue(T val){
+            return LL.addLast(val);
+};
+
+template <typename T>
+T* QueueLinkedList<T>::dequeue(){
+    return LL.popFront();
+}
+
+template <typename T>
+bool QueueLinkedList<T>::isEmpty(){
+    return LL.isEmpty();
+}
+
+template <typename T>
+void QueueLinkedList<T>::print(){
+    LL.print();
+}
+
+
+
+/*Queue class using array implementation*/
+template <typename T> 
 class QueueArray{
     private:
         T read = 0,write = 0; //index
@@ -149,87 +197,114 @@ class QueueArray{
         int mod(int a, int b) { return (a % b + b) % b; }
         
     public:
-        bool enqueue(T val){
-            if(isFull()){
-                return false;
-            }
-            arr[write] = val;
-            write = (write+1) % maxLen;
-            
-            return true;
-        }
+        bool enqueue(T val);
         
-        T * dequeue(){
-            T *valptr;
-            if(isEmpty()){
-                valptr = nullptr;
-            } else {
-                auto val = arr[read];
-                read = mod((read+1),maxLen);
-                valptr = &val;
-            }
-
-            return valptr;
-        }
+        T * dequeue();
         
-        bool isEmpty(){
-            return (read == write);
-        }
+        bool isEmpty();
         
-        bool isFull(){
-            return (mod(read -1,maxLen) == mod(write,maxLen));
-        }
+        bool isFull();
         
-        void print(){
-            int i = read;
-            while(i != write){
-                cout<<arr[i]<<"<-";
-                i = mod((i+1) , maxLen);
-            }
-            cout<<endl;
-        }
-        
+        void print();
 };
 
+template <typename T>
+bool QueueArray<T>::enqueue(T val){
+    if(isFull()){
+        return false;
+    }
+    arr[write] = val;
+    write = (write+1) % maxLen;
+    
+    return true;
+};
 
-template <typename T> //Queue class for array implementation
+template <typename T>
+T* QueueArray<T>::dequeue(){
+    T *valptr;
+    if(isEmpty()){
+        valptr = nullptr;
+    } else {
+        auto val = arr[read];
+        read = mod((read+1),maxLen);
+        valptr = &val;
+    }
+
+    return valptr;
+};
+
+template <typename T>
+bool QueueArray<T>::isEmpty(){
+    return (read == write);
+};
+
+template <typename T>
+bool QueueArray<T>::isFull(){
+    return (mod(read -1,maxLen) == mod(write,maxLen));
+}
+
+template <typename T>
+void QueueArray<T>::print(){
+    int i = read;
+    while(i != write){
+        cout<<arr[i]<<"<-";
+        i = mod((i+1) , maxLen);
+    }
+    cout<<endl;
+}
+
+
+
+/*Queue class using dynamic array implementation*/
+template <typename T>
 class QueueDynamicArray{
     private:
 
         vector<T> arr;
         
     public:
-        bool enqueue(T val){
-            arr.insert(arr.end(),val);
-            return true;
-        }
+        bool enqueue(T val);
         
-        T * dequeue(){
-            T *valptr;
-            if(isEmpty()){
-                valptr = nullptr;
-            } else {
-                T val =  *arr.begin();
-                valptr = &val;
-                arr.erase(arr.begin());
-            }
-
-            return valptr;
-        }
+        T * dequeue();
         
-        bool isEmpty(){
-            return arr.empty();
-        }
+        bool isEmpty();
         
-        void print(){
-            for(auto i = arr.begin(); i != arr.end(); i++){
-                cout << *i << "<-";
-            }
-            cout<<endl;
-        }
+        void print();
         
 };
 
+template <typename T>
+bool QueueDynamicArray<T>::enqueue(T val){
+            arr.insert(arr.end(),val);
+            return true;
+};
+
+template <typename T>
+T* QueueDynamicArray<T>::dequeue(){
+    T *valptr;
+    if(isEmpty()){
+        valptr = nullptr;
+    } else {
+        T val =  *arr.begin();
+        valptr = &val;
+        arr.erase(arr.begin());
+    }
+
+    return valptr;
+};
+
+template <typename T>
+bool QueueDynamicArray<T>::isEmpty(){
+    return arr.empty();
+};
+
+template <typename T>
+void QueueDynamicArray<T>::print(){
+    for(auto i = arr.begin(); i != arr.end(); i++){
+        cout << *i << "<-";
+    }
+    cout<<endl;
+};
 
 int main() {
     
@@ -283,9 +358,5 @@ int main() {
     QDR.print();
     return 0;
 }
-
-
-
-
 
 
